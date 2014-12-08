c6e2aabfde286894729336662ba0784211aa8613
Khalah Jones Golden <khasan222@gmail.com>
2014-12-08 15:03:41 -0500

    Added some cool effects, tested in different browsers. Almost done

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 $(function() {
+    // for reference
+    var $window = $(window),
+        windowHeight = $window.innerHeight();
+
+
     var fullHeight = function() {
-        var pageHeight = $(window).innerHeight();
         $('.full-height').each(function(idx, el) {
-            $(el).css('height', pageHeight + 'px');
+            $(el).css('height', windowHeight + 'px');
         });
-    }();
+    };
 
     function jumbotronImage() {
-        var $doc = $(window),
-            $jumboImg = $('.main.img'),
+        var $jumboImg = $('.main.img'),
             pageDimensions = {
-                height: $doc.innerHeight(),
-                width: $doc.innerWidth()
+                height: windowHeight,
+                width: $window.innerWidth()
             };
         // if with 100% height the image isn't wide enough
         // 16:9 screens
@@ -21,11 +24,65 @@ $(function() {
             $jumboImg.height(pageDimensions.height);
         }
     }
-
-    function fadeIn() {
-        $('.fade-in').addClass('in');
-    }
-
+    // run the functions
+    fullHeight();
     jumbotronImage();
-    setTimeout(fadeIn, 300);
+
+    // add a resize element since these are dependent on window size
+    $window.on('resize', function() {
+        windowHeight = $window.innerHeight();
+        fullHeight();
+        jumbotronImage();
+    });
+
+    (function() {
+        var $animated = [],
+            offsets = {},
+            $lastSection = null,
+            lastScrollIdx = -1;
+
+        // animate in all the elements of a particular section
+        function animateInElements($sec) {
+            $sec.find('[data-effect]').each(function(i, el) {
+                var $el = $(el);
+                $el
+                    .removeClass('invisible')
+                    .addClass('animated ' + $el.data('effect'));
+            });
+        }
+
+        // go through each element, and prepare them to be animated in.
+        $('[data-effect]').each(function(idx, el) {
+            var $el = $(el),
+                $section = $el.closest('section, .jumbotron');
+            $el.addClass('invisible');
+            if(!$section.is($lastSection)) {
+                // mark down the position of each elements section maybe
+                $animated.push($section);
+                // maybe just store the top if we don't need anything else
+                offsets[$section.attr('id')] = $section.offset().top + $section.height() * .8; // don't start the animation until we're mostly through the page
+            }
+            $lastSection = $section;
+        });
+
+        console.log(offsets);
+
+        setTimeout(function() {
+            // on scroll check and see if an element is visible, if so animate it in
+            $window.on('scroll', function(evt) {
+                $animated = $animated.filter(function($el) {
+                    var animatedAlready = false;
+                    if(offsets[$el.attr('id')] < $window.scrollTop() + windowHeight) {
+                        animateInElements($el);// remove the items so they don't get the effect two fold
+                        animatedAlready = true;
+                    }
+                    return !animatedAlready;
+                });
+            });
+
+            //fade in the jumbotron and such
+            $window.trigger('scroll');
+        }, 100);
+    }());
+
 });
