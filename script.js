ec3600614c399cd4b48d9ac096c9f2b6486daa95
Khalah Jones Golden <khasan222@gmail.com>
2015-01-15 06:54:06 -0400

    added a loading gif, fixed some punctuation problems, added some new styles, cleanup and general bug fixes in js and html

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ $(function() {
     var $window = $(window),
         windowHeight = $window.innerHeight();
 
-
+    // make the jumbotron (image and name) take up the whole screen
     var fullHeight = function() {
         $('.full-height').each(function(idx, el) {
             $(el).css('height', windowHeight + 'px');
         });
     };
 
+    // make sure the jumbotron image takes up the whole screen
     function jumbotronImage() {
         var $jumboImg = $('.main.img'),
             pageDimensions = {
@@ -28,12 +29,7 @@ $(function() {
     fullHeight();
     jumbotronImage();
 
-    // add a resize element since these are dependent on window size
-    $window.on('resize', function() {
-        windowHeight = $window.innerHeight();
-        fullHeight();
-        jumbotronImage();
-    });
+    // add an event so that on window load the loading image is taken back down
 
     (function() {
         var $animated = [],
@@ -54,35 +50,52 @@ $(function() {
         // go through each element, and prepare them to be animated in.
         $('[data-effect]').each(function(idx, el) {
             var $el = $(el),
-                $section = $el.closest('section, .jumbotron');
+                $section = $el.closest('section, .jumbotron'),
+                offsetTop = $section.offset().top;
             $el.addClass('invisible');
             if(!$section.is($lastSection)) {
                 // mark down the position of each elements section maybe
                 $animated.push($section);
                 // maybe just store the top if we don't need anything else
-                offsets[$section.attr('id')] = $section.offset().top + $section.height() * .8; // don't start the animation until we're mostly through the page
+                offsets[$section.attr('id')] = [offsetTop, offsetTop + $section.height() * .8]; // don't start the animation until we're mostly through the page
             }
             $lastSection = $section;
         });
 
-        console.log(offsets);
-
-        setTimeout(function() {
-            // on scroll check and see if an element is visible, if so animate it in
-            $window.on('scroll', function(evt) {
+        // add a resize element since these are dependent on window size
+        $window.on({
+            resize: function() {
+                windowHeight = $window.innerHeight();
+                fullHeight();
+                jumbotronImage();
+            },
+            // TODO throttle this in case more speed is needed
+            scroll: function(evt) {
+                var scrollTop = $window.scrollTop();
+                // filter down the animated elements so we don't waste iterations
                 $animated = $animated.filter(function($el) {
-                    var animatedAlready = false;
-                    if(offsets[$el.attr('id')] < $window.scrollTop() + windowHeight) {
-                        animateInElements($el);// remove the items so they don't get the effect two fold
+                    var animatedAlready = false,
+                        offset = offsets[$el.attr('id')];
+                    if(offset[0] < scrollTop + windowHeight * .5 || offset[1] <= scrollTop + windowHeight) {     // if the top of the section is halfway up the screen OR if the bottom of the section is visible
+                        // add the classes to the elements so they animate in properly
+                        animateInElements($el);
+                        // set a flag so we remove this from the list of things to be animated
                         animatedAlready = true;
                     }
                     return !animatedAlready;
                 });
-            });
+                // stop the scroll event on the window if all of the animations have been done
+                if($animated.length === 0) {
+                    $window.off('scroll');
+                }
+            },
+            load: function() {
+                $("#loading-box").addClass("animated fadeOutUp");
 
-            //fade in the jumbotron and such
-            $window.trigger('scroll');
-        }, 100);
+                //fade in the jumbotron and such
+                $window.trigger('scroll');
+            }
+        });
     }());
 
 });
